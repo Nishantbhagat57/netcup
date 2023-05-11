fcc8113eb4300ed8bb83aca120d42be6cda216c6
Nishant Bhagat <64642374+Nishantbhagat57@users.noreply.github.com>
2023-05-11 20:58:08 +0530

    Create server.sh

---
+++ server.sh
@@ -0,0 +1,6 @@
+#! /bin/bash
+sudo apt update
+wget https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb
+sudo apt-get install --assume-yes ./chrome-remote-desktop_current_amd64.deb
+sudo apt install xfce4  xfce4-goodies -y
+sudo apt update && sudo apt upgrade
